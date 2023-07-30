808a24bc6672e470e26d94c9829b0557329e1adb
KramchaninAlena <100025327+KramchaninAlena@users.noreply.github.com>
2023-07-30 12:13:56 +0300

    task_2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import "./styles/normalize.css";
 import "./styles/index.css";
-import { getAllProducts } from "./requests/products";
+import { getAllProducts, getProductById } from "./requests/products";
 import {
   createMarkupAllProducts,
   renderAllProducts,
 } from "./services/markupService";
+import { refs } from "./services/refs";
+import { handleSubmitForm } from "./js/handleSubmitForm";
 
-getAllProducts()
-  .then(createMarkupAllProducts)
-  .then(renderAllProducts)
-  .catch(console.log);
+
+
+// getAllProducts()
+//   .then(createMarkupAllProducts)
+//   .then(renderAllProducts)
+//   .catch(console.log);
+
+refs.formProducts.addEventListener('submit', handleSubmitForm);
+
--- src/services/markupService.js
+++ src/services/markupService.js
@@ -9,4 +9,12 @@ export function createMarkupAllProducts(object) {
 
 export function renderAllProducts(e) {
   refs.ulProductsEl.insertAdjacentHTML("beforeend", e);
+};
+
+export function createMarkupProduct({ title, price, description }) {
+  return `<h3>${title}</h3> <p>price: ${price}</p> <p>description: ${description}</p>`
+};
+
+export function renderProduct(product) {
+  refs.containerProducts.innerHTML = product;
 }
